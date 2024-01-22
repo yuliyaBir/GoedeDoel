4849cb0242b0d155435406fd4725f229e7d9d694
yuliyaBir <zhivo4ka@gmail.com>
2024-01-22 10:20:48 +0100

    Initial commit

---
+++ src/main/java/be/vdab/goededoel/GoedeDoelApplication.java
@@ -0,0 +1,13 @@
+package be.vdab.goededoel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoedeDoelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoedeDoelApplication.class, args);
+	}
+
+}
