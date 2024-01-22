38db828e1465f481b73637539f455f1e79d05cb0
yuliyaBir <zhivo4ka@gmail.com>
2024-01-22 13:32:50 +0100

    HashCode toegevoegd

--- src/main/java/be/vdab/goededoel/GoedeDoel.java
+++ src/main/java/be/vdab/goededoel/GoedeDoel.java
@@ -21,4 +21,8 @@ public class GoedeDoel {
 	public boolean equals(Object object) {
 		return object instanceof GoedeDoel ander && naam.equalsIgnoreCase(ander.naam);
 	}
+	@Override
+	public int hashCode() {
+		return this.naam.toUpperCase().hashCode();
+	}
 }
--- src/test/java/be/vdab/goededoel/GoedeDoelTest.java
+++ src/test/java/be/vdab/goededoel/GoedeDoelTest.java
@@ -38,5 +38,9 @@ class GoedeDoelTest {
 	void doelVerschiltVanEenObjectMetEenAnderType() {
 		assertThat(doel).isNotEqualTo(BigDecimal.ZERO);
 	}
+	@Test
+	void gelijkeDoelGevenHebbenDezelfdeHashCode() {
+		assertThat(doel).hasSameHashCodeAs(new GoedeDoel(NAAM));
+	}
 
 }
